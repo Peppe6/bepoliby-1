d7916205b48d419a6fd1dc03fb30c247359982d2
Peppe6 <95749196+Peppe6@users.noreply.github.com>
2025-07-03 00:24:26 +0200

    Add files via upload

--- bepoliby-fe/src/App.js
+++ bepoliby-fe/src/App.js
@@ -6,7 +6,7 @@ import Chat from './Chat/Chat';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Avatar from "@mui/material/Avatar";
 import { useStateValue } from './StateProvider';
-import jwtDecode from "jwt-decode";
+import jwtdecode from "jwt-decode";
 
 function InfoCenter() {
   const [{ user }] = useStateValue();
@@ -63,7 +63,7 @@ function App() {
       if (!token) return;
 
       try {
-        const decoded = jwtDecode(token);
+        const decoded = jwtdecode(token);
         const { id, nome, username } = decoded;
 
         if (!id || !nome || !username) return;
