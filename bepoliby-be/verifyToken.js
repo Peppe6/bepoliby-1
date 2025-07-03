2cd2bfd3620da6a61c4656e7ba9953a4ae43eba6
Peppe6 <95749196+Peppe6@users.noreply.github.com>
2025-07-04 00:47:36 +0200

    Update verifyToken.js

--- bepoliby-be/verifyToken.js
+++ bepoliby-be/verifyToken.js
@@ -1,16 +1,20 @@
-
-const jwt = require('jsonwebtoken');
-
 function verifyToken(req, res, next) {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.status(401).json({ message: "No token provided" });
+  if (!authHeader) {
+    console.log("No authorization header");
+    return res.status(401).json({ message: "No token provided" });
+  }
 
   const token = authHeader.split(' ')[1];
-  if (!token) return res.status(401).json({ message: "No token provided" });
+  if (!token) {
+    console.log("No token in header");
+    return res.status(401).json({ message: "No token provided" });
+  }
 
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET);
-    // Rimappo i campi per coerenza con il resto del codice
+    console.log("User decoded from token:", user);
+
     req.user = {
       uid: user.id || user.uid,
       nome: user.name || user.nome,
@@ -18,8 +22,7 @@ function verifyToken(req, res, next) {
     };
     next();
   } catch (err) {
+    console.log("Token invalid:", err.message);
     return res.status(403).json({ message: "Invalid token" });
   }
 }
-
-module.exports = verifyToken;
