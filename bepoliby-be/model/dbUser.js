66ed037ab00fcc9d722d376cbf372013521ee41f
Peppe6 <95749196+Peppe6@users.noreply.github.com>
2025-07-04 13:08:05 +0200

    Update dbUser.js

--- bepoliby-be/model/dbUser.js
+++ bepoliby-be/model/dbUser.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  uid: { type: String, required: true, unique: true },  // UID unico
+  uid: { type: String, required: true, unique: true },
   nome: { type: String, required: true },
   username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
-  // password, avatar, ecc.
 }, {
   timestamps: true
 });
 
-module.exports = mongoose.model('User', userSchema);
