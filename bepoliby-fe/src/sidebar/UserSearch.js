c3255742add5b61b331f4ee519ab5414a8d2b898
Peppe6 <95749196+Peppe6@users.noreply.github.com>
2025-07-08 12:50:29 +0200

    Update UserSearch.js

--- bepoliby-fe/src/sidebar/UserSearch.js
+++ bepoliby-fe/src/sidebar/UserSearch.js
@@ -1,4 +1,4 @@
-mo devo fare questa modifca anche qui:import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './UserSearch.css';
 import { Avatar } from '@mui/material';
 
